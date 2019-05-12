e6f7f205d7a209a0e4880d58f15464f3c0a8361c
zuzia-kru <zuzia.kru@gmail.com>
2019-05-12 15:19:32 +0200

    First commit.

---
+++ app/src/main/java/com/example/wikipedia/activities/ArticleDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wikipedia.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.wikipedia.R
+
+class ArticleDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
---
+++ app/src/main/java/com/example/wikipedia/activities/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wikipedia.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wikipedia.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
